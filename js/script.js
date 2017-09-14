8b5a5524009700333a5029b3213dc976cb7d6b79
Sturzael <esturz23@gmail.com>
2017-09-15 11:32:21 +1200

    trying to set up menu functionality/end of day push

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-$(window).scroll(function(){
-    if ($(this).scrollTop() > 5) {
-      $('.Menu').css("backgroundColor", "black");
-
-    } else if ($(this).scrollTop() < 5){
-          $('.Menu').css("backgroundColor", "transparent");
-
-    }
-});
+// $(window).scroll(function(){
+//     if ($(this).scrollTop() > 5) {
+//       jQuery(".Menu").animate({
+//   			backgroundColor: "blue"
+//   	}, 200 );
+//
+//     } else if ($(this).scrollTop() < 5){
+//       jQuery(".Menu").animate({
+//         backgroundColor: "transparent"
+//     }, 200 );
+//     }
+// });
