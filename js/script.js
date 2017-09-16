89d58f8cd4674b869da616c546f515ac967d9299
Sturzael <esturz23@gmail.com>
2017-09-16 19:20:49 +1200

    tidying up page layout, creating menu, positioned info div

--- js/script.js
+++ js/script.js
@@ -1,12 +1,44 @@
-// $(window).scroll(function(){
-//     if ($(this).scrollTop() > 5) {
-//       jQuery(".Menu").animate({
-//   			backgroundColor: "blue"
-//   	}, 200 );
-//
-//     } else if ($(this).scrollTop() < 5){
-//       jQuery(".Menu").animate({
-//         backgroundColor: "transparent"
-//     }, 200 );
-//     }
-// });
+
+
+	$('#nav-icon2').click(function(){
+		$(this).toggleClass('open');
+	});
+
+var pagetop, menu, yPos, logo;
+var hamburger = document.getElementById("nav-icon2").querySelectorAll(".spanner");
+for (var i = 0; i < hamburger.length; i++) {
+  hamburger[i]
+}
+window.addEventListener("scroll", yScroll);
+function yScroll(){
+  logo = document.getElementById('logo');
+	pagetop = document.getElementById('pagetop');
+	menu = document.getElementById('menu');
+
+	yPos = window.pageYOffset;
+	if(yPos > 50){
+      pagetop.style.backgroundColor ="#fff";
+		pagetop.style.height = "90px";
+		pagetop.style.paddingTop = "8px";
+      logo.style.color="#DA1932";
+      logo.style.fontSize="60px";
+      for (var i = 0; i < hamburger.length; i++) {
+        hamburger[i].style.backgroundColor= "#22327B";
+      }
+
+
+
+
+	} else {
+  pagetop.style.backgroundColor ="transparent";
+		pagetop.style.height = "120px";
+		pagetop.style.paddingTop = "30px";
+    logo.style.color="#fff";
+    logo.style.fontSize="100px";
+    for (var i = 0; i < hamburger.length; i++) {
+      hamburger[i].style.backgroundColor= "#fff";
+    }
+
+
+	}
+}
