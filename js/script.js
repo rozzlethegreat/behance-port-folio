e01070666ae640215253aec9b96b179f908a01d0
Sturzael <esturz23@gmail.com>
2017-09-28 18:51:44 +1300

    FINAL PUSH

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-
-
-
-
-var pagetop, menu, yPos, logo, APIKey, DesignersProfile, first_name, last_name, city, identity, owner, owner2, owner3, url, url2;
+var pagetop, menu, yPos, logo, APIKey, DesignersProfile, first_name, last_name, city, identity, owner, owner2, owner3, url, url2, id, first_name, last_name, city, dp, projectscover;
 var artistName = [],
   workName = [],
   views = [],
@@ -19,20 +15,13 @@ for (var i = 0; i < hamburger.length; i++) {
   hamburger[i]
 }
 window.addEventListener("scroll", yScroll);
-
-//Smooth Scrolling with in page anchors
 $("a").on('click', function(event) {
   $('.MenuOpen').css("display", "none");
   $('#nav-icon2').toggleClass('open');
 
   if (this.hash !== "") {
-
     event.preventDefault();
-
-
     var hash = this.hash;
-
-
     $('html, body').animate({
       scrollTop: $(hash).offset().top
     }, 800, function() {
@@ -83,7 +72,7 @@ $('.grid').masonry({
   itemSelector: '.grid-item',
   columnWidth: 200
 });
-// ***REQUESTS*** //
+
 
 $.ajax({
   url: "./config.json",
@@ -104,7 +93,7 @@ $.ajax({
     console.log("Something Went Wrong");
   }
 })
-var id, first_name, last_name, city, dp, projectscover
+
 
 function getRippedDesigners() {
   $.ajax({
@@ -114,7 +103,6 @@ function getRippedDesigners() {
       for (var i = 0; i < DataFromJson.following.length; i++) {
         designerIDs.push(DataFromJson.following[i]);
       };
-
       getProjects();
     },
     error: function() {
@@ -131,16 +119,11 @@ function getProjects() {
       url: "http://www.behance.net/v2/users/" + designerIDs[i].id + "/projects?client_id=" + APIKey,
       dataType: "jsonp",
       success: function(DataFromJson) {
-
         projectIDs.push(DataFromJson.projects[0].id);
-
-        // for (var i = 0; i < 9; i++) {
         projects.push(DataFromJson);
-        // .projects[i].owners[0].first_name[DataFromJson.projects[i].id, DataFromJson.projects[i].name, DataFromJson.projects[i].covers.original, ]
         if (projectIDs.length > 11) {
           getProjectImg();
         };
-
         var sArtistName = DataFromJson.projects[0].owners[0].display_name;
         var sViews = DataFromJson.projects[0].stats.views;
         var sAppreciations = DataFromJson.projects[0].stats.appreciations;
@@ -155,46 +138,33 @@ function getProjects() {
           'packages': ['table']
         });
         google.charts.setOnLoadCallback(drawTable);
-
       },
       error: function() {
         console.log("Something Went Wrong");
-
       }
-
     })
   }
-
 }
 
 function getProjectImg() {
   for (var i = 0; i < projectIDs.length; i++) {
     var gridItems = [];
     gridItems.push('grid-item' + i);
-
-
     $.ajax({
       url: "http://www.behance.net/v2/projects/" + projectIDs[i] + "?client_id=" + APIKey,
       dataType: "jsonp",
       success: function(DataFromJson) {
-
         $('.grid').append("<div class='grid-item img-tag'><a id='aLink' data-remodal-target='modal'><img class='img grid-item img-tag' style='background-image: url(" + DataFromJson.project.covers.original + ")'></img><h1 class='Dets'>" + DataFromJson.project.owners[0].first_name + "</h1></a></div>");
-
         if ($('.grid-item').length >= 24) {
-
           hoverEffect();
           getStuff();
-
         } else {
           return;
         }
-
       },
       error: function() {
         console.log("Something Went Wrong");
-
       }
-
     })
   }
 }
@@ -202,7 +172,6 @@ function getProjectImg() {
 
 function hoverEffect() {
   var boxes = document.querySelectorAll('.grid-item');
-
   for (var i = 0; i < boxes.length; i++) {
     boxes[i].style.height = boxes[i].offsetWidth + 'px';
     boxes[i].addEventListener("mouseenter", function(e) {
@@ -231,8 +200,6 @@ function getStuff() {
   $('.grid-item').click(onClick);
 
   function onClick() {
-
-
     for (var i = 0; i < 12; i++) {
       owner = $(this).find('.Dets').html();
       if (owner == designerIDs[i].first_name || owner2 == designerIDs[i].first_name) {
@@ -243,13 +210,19 @@ function getStuff() {
         last_name = designerIDs[i].last_name;
         city = designerIDs[i].city;
         followers = designerIDs[i].stats.followers.toLocaleString(
-          undefined,{minimumFractionDigits: 0}
+          undefined, {
+            minimumFractionDigits: 0
+          }
         );
         appris = designerIDs[i].stats.appreciations.toLocaleString(
-          undefined,{minimumFractionDigits: 0}
+          undefined, {
+            minimumFractionDigits: 0
+          }
         );
         views = designerIDs[i].stats.views.toLocaleString(
-          undefined,{minimumFractionDigits: 0}
+          undefined, {
+            minimumFractionDigits: 0
+          }
         );
         $('.circlePic').empty();
         if (dp !== undefined) {
@@ -259,10 +232,8 @@ function getStuff() {
         }
         $('#changed').empty();
         $('#changed').append('View Profile');
-
         $('#User').empty();
         $('#User').append(first_name + " " + last_name);
-
         $('#city').empty();
         $('#city').append("<i class='fa fa-map-marker icons'></i><p class='city'>" + city + "</p>");
         $('#views').empty();
@@ -273,21 +244,14 @@ function getStuff() {
         $('#followers').append("<i class='fa fa-users icons'></i><p class='city'>" + followers + "</p>");
         projectIden = [];
         $('.box').empty();
-
-
-
         url = designerIDs[i].url
-
-
         for (var i = 0; i < 12; i++) {
           if (owner == projects[i].projects[0].owners[0].first_name || owner2 == projects[i].projects[0].owners[0].first_name) {
             owner2 = "";
             for (var j = 0; j < 8; j++) {
               $('.box').append("<div class='project' style='background-image: url(" + projects[i].projects[j].covers[404] + ")'></div>");
               projectIdss = projects[i].projects[j].id;
-
               projectIden.push(projects[i].projects[j]);
-
             }
           }
         }
@@ -323,24 +287,24 @@ function getimg() {
             $('#changed').append('View Project');
             $('#User').empty();
             $('#User').append(DataFromJson.project.name);
-
             appris = DataFromJson.project.stats.appreciations.toLocaleString(
-              undefined,{minimumFractionDigits: 0}
+              undefined, {
+                minimumFractionDigits: 0
+              }
             );
-            views =  DataFromJson.project.stats.views.toLocaleString(
-              undefined,{minimumFractionDigits: 0}
+            views = DataFromJson.project.stats.views.toLocaleString(
+              undefined, {
+                minimumFractionDigits: 0
+              }
             );
             $('#city').empty();
-            $('#city').append("<p class='city'>"+DataFromJson.project.owners[0].display_name+"</p>");
+            $('#city').append("<p class='city'>" + DataFromJson.project.owners[0].display_name + "</p>");
             $('#views').empty();
             $('#views').append("<i class='fa fa-eye icons'></i><p class='city'>" + views + "</p>");
             $('#appris').empty();
             $('#appris').append("<i class='fa fa-heart icons'></i><p class='city'>" + appris + "</p>");
             $('.box').empty();
-
             owner3 = DataFromJson.project.owners[0].first_name;
-
-
             $('#followers').empty();
             $('.projectText').css('color', 'black');
             $('.projectText').css("margin-top", "0px");
@@ -349,10 +313,8 @@ function getimg() {
               window.location.href = url2;
             });
             for (var i = 0; i < DataFromJson.project.modules.length; i++) {
-
               $('.box').css("background-color", "#" + DataFromJson.project.styles.background.color);
               $('.projectItem').css("margin-bottom", DataFromJson.project.styles.spacing.modules.bottom_margin + "px");
-
               if (DataFromJson.project.modules[i].type == "text") {
                 $('.projectText').css('color', DataFromJson.project.styles.text.paragraph.color);
                 $('.projectText').css('font-size', DataFromJson.project.styles.text.paragraph.font_size);
@@ -369,17 +331,11 @@ function getimg() {
           },
           error: function() {
             console.log("Something Went Wrong");
-
           }
-
         })
       }
     }
-
   })
-
-
-
 }
 
 function drawTable() {
@@ -390,7 +346,6 @@ function drawTable() {
     'selectedTableRow': 'selected',
     'hoverTableRow': 'red-color'
   };
-
 
   var options = {
     'showRowNumber': true,
@@ -411,15 +366,15 @@ function drawTable() {
 
   table.draw(data, options);
 }
-$('#statsButton').on("click", function(){
+$('#statsButton').on("click", function() {
   $('#table-div').css("display", "block");
   $('.footer').css("height", "40vh")
-$('#statsButton').css("display", "none");
-$('#HideButton').css("display", "block");
+  $('#statsButton').css("display", "none");
+  $('#HideButton').css("display", "block");
 })
-$('#HideButton').on("click", function(){
+$('#HideButton').on("click", function() {
   $('#table-div').css("display", "none");
   $('.footer').css("height", "25vh")
-$('#HideButton').css("display", "none");
-$('#statsButton').css("display", "block");
+  $('#HideButton').css("display", "none");
+  $('#statsButton').css("display", "block");
 });
